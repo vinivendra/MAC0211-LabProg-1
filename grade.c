5911104e680e4671361dec27f12e3b4219078e3e
fllsouto <fllsouto@gmail.com>
2013-05-07 19:41:41 -0300

    add do method aleatorizamargem e proximoframe

--- grade.c
+++ grade.c
@@ -23,3 +23,34 @@ void criaPrimeiroFrame(float **grade, int altura, int largura, float limiteDasMa
         primeiraLinha(grade[i], largura, limiteDasMargens, fluxoDesejado);
     }
 }
+
+
+void criaProximoFrame (float **grade, int altura, int largura, float limiteDasMargens, int fluxoDesejado){
+  
+    float i,*tmp = grade[0];
+  
+  for (i = 0; i < largura - 1; i++)
+      grade[i] = grade[i+1];
+  
+  aleatorizaMargem (grade[largura-1], tmp,limiteDasMargens);
+  grade[largura -1] = tmp;
+  
+}
+
+void aleatorizaMargem(float *lLine, float *fLine, int limiteDasMargens,largura){
+    
+    int tamanhoDaMargemEsquerda = margemEsquerda(lLine);
+    int tamanhoDaMargemDireita = margemDireita(lLine, largura);
+    int resultado1 = (rand()%3) - 1;
+    int resultado2 = (rand()%3) - 1;
+    int tempMargemEsq = tamanhoDaMargemEsquerda + resultado1;
+    int tempMargemDir = tamanhoDaMargemDireita + resultado2;
+    int i;
+    
+    for (i = 0; i < tempMargemEsq; i++) /* Insere nova margem esquerda */
+	fline[i] = 0;
+	
+
+    for (i = largura - tempMargemDir; i < largura; i++) { /* Insere nova margem direita */
+	fline[i] = 0;
+  }
