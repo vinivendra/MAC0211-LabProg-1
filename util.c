f4de07ed2268106abe8dabaf02ddd8bc6edafa28
Gervasio Santos <gervasio.neto94@gmail.com>
2013-05-09 23:45:02 -0300

    Resolvidos alguns problemas que ainda persistiam. Criado um .c e um .h especificiamente para getArgs

---
+++ util.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "util.h"
+
+void getArgs(int argc,char *argv[],int *velocidadeDoBarco, int *larguraDoRio, int *seed, int *fluxoDesejado, 
+	     int *verbose, int *dIlha, float *pIlha)
+{
+  int read = 0;
+  
+  while(--argc){
+    if (sscanf(argv[argc], "-b%d",velocidadeDoBarco)) read++;
+    else if (sscanf(argv[argc], "-l%d",larguraDoRio)) read++;
+    else if (sscanf(argv[argc], "-s%d",seed)) read++;
+    else if (sscanf(argv[argc], "-f%d",fluxoDesejado)) read++;
+    else if (sscanf(argv[argc], "-pI%f",pIlha)) read++;
+    else if (sscanf(argv[argc], "-dI%d",dIlha)) read++;
+    else if (strcmp(argv[argc],"-L") == 0)
+      *verbose = 1;
+    else {
+      printf ("\t \t Opcoes disponiveis: \n"
+	      "-bxxx  - Velocidade do barco\n"
+	      "-lxxx  - Largura do Rio\n"
+	      "-sxxx  - semente para o gerador aleatorio\n"
+	      "-fxxx  - Fluxo da agua\n"
+	      "-pIxxx - Probabilidade de haver obstaculos\n"
+	      "-dIxxx - Distancia minima entre obstaculos\n");
+      exit (-1);
+    }
+  }
+}
---
+++ util.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void getArgs(int argc,char *argv[],int *, int *, int *, int *, int *, int *, float *);
